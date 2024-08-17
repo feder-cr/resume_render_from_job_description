4457f3e307456295f61f1ae8f77b248af3d0fa5f
feder-cr <federico.elia.majo@gmail.com>
2024-08-17 02:23:15 +0100

    using lib_resume_builder_AIHawk

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import base64
 from email import utils
 import os
 from pathlib import Path
 import yaml
-from src.manager_facade import FacadeManager
-from src.config import Config
-from src.resume import Resume
-from src.style_manager import StyleManager
-from src.resume_generator import ResumeGenerator
+from lib_resume_builder_AIHawk import Resume, StyleManager, ResumeGenerator, FacadeManager
+
 
 @staticmethod
 def validate_secrets(secrets_yaml_path: Path):
@@ -29,24 +27,21 @@ def main():
     folder = "log"
     if not os.path.exists(folder):
         os.makedirs(folder)
+    log_path = Path(folder).resolve()
 
     api_key = validate_secrets(Path("secrets.yaml"))
-    config = Config(
-        STRINGS_MODULE_RESUME_PATH=Path("resume_prompt/strings_feder-cr.py").resolve(),
-        STRINGS_MODULE_RESUME_JOB_DESCRIPTION_PATH=Path("resume_job_description_prompt/strings_feder-cr.py").resolve(),
-        STRINGS_MODULE_NAME="strings_feder_cr",
-        STYLES_DIRECTORY=Path("resume_style").resolve(),
-        RESUME_TEMPLATE_PATH=Path("plain_text_resume.yaml").resolve(),
-        OUTPUT_FILE_PATH=Path("resume.html").resolve(),
-        API_KEY=api_key
-    )
-    with open(config.RESUME_TEMPLATE_PATH, "r") as file:
+
+    with open("plain_text_resume.yaml", "r") as file:
         plain_text_resume = file.read()
         resume_object = Resume(plain_text_resume)
+
     style_manager = StyleManager()
     resume_generator = ResumeGenerator()
-    manager = FacadeManager(config, style_manager, resume_generator, resume_object)
-    manager.run()    
+    manager = FacadeManager(api_key, style_manager, resume_generator, resume_object, log_path)
+    if os.path.exists("resume.pdf"):
+        os.remove("resume.pdf")
+    with open("resume.pdf", "xb") as f:
+        f.write(base64.b64decode(manager.pdf_base64()))
 
 if __name__ == "__main__":
     main()
